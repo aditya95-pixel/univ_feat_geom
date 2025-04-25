ac39f3576514b608417e2f81611a8ad7f68fefa9
Aditya Mazumdar <136506411+aditya95-pixel@users.noreply.github.com>
2025-04-25 12:56:47 +0530

    Update dataset_breakdown.py

--- run_pipeline/dataset_breakdown.py
+++ run_pipeline/dataset_breakdown.py
@@ -3,10 +3,10 @@ from datasets import load_dataset
 # Load full dataset
 dataset = load_dataset("stas/openwebtext-10k", split="train", streaming=False)
 
-# Shuffle and select first 1000 samples
-small_dataset = dataset.shuffle(seed=42).select(range(1000))
+# Shuffle and select first 200 samples
+small_dataset = dataset.shuffle(seed=42).select(range(200))
 
 # Save to disk
-save_path = "openwebtext-1k"
+save_path = "openwebtext-200"
 small_dataset.save_to_disk(save_path)
 print(f"Saved to {save_path}")
